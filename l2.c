789fbf579002f90d0a47cf6140a729d904a1e6b5
amircs2004 <saidimahdiamir@gmail.com>
2024-10-30 21:13:23 +0100

    ee

---
+++ l2.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+  struct node* prev ;
+  int data ; 
+  struct node *next;
+};
+struct node* addToEmpty(int data){
+  struct node* temp = malloc(sizeof (struct node));
+  temp->prev = temp;
+  temp->data = data ;
+  temp->next = temp;
+}
+struct node* addAtBeg(struct node* tail , int data)
+{
+   struct node* newp = addToEmpty(data);
+     if (tail == NULL)
+     {
+      return newp ;
+     }
+    else {
+      struct node * temp = tail->next ;
+      newp->prev = tail;
+      newp->next = temp;
+      temp->prev = newp;
+      tail->next = newp ;
+      return tail;
+    }
+}
+void print(struct node* tail){
+  if (tail == NULL)
+  {
+    printf("no element in the fucking list ");
+  }
+   else{
+    struct node * temp = tail->next ;
+    do
+    {
+      printf("%d", temp->data);
+      temp = temp->next ;
+    }while(temp != tail->next);
+   }
+printf("\n");
+}
+
+int main()
+{
+  struct node * tail = NULL ;
+  tail = addToEmpty(45);
+  tail = addAtBeg(tail , 34);
+  print(tail);
+  return 0;
+}
